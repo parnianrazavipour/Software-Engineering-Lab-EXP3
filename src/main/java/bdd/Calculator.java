6ac73706709da4a1dcaae964093d5b3cec13d252
DnyaNvB <nvbdnya@gmail.com>
2024-11-22 23:59:27 +0330

    add "*" operator

--- src/main/java/bdd/Calculator.java
+++ src/main/java/bdd/Calculator.java
@@ -6,6 +6,8 @@ public class Calculator {
             return operand1 + operand2;
         } else if (operator.equals("-")) {
             return operand1 - operand2;
+        } else if (operator.equals("*")) {
+            return operand1 * operand2;
         } else {
             throw new IllegalArgumentException("Invalid operator: " + operator);
         }
