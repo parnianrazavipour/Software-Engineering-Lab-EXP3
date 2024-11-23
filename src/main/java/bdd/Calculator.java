7e738da656521e3ee1ad5fb5d4d0f1c59083d725
Parnian <razaviparnian81@yahoo.com>
2024-11-23 12:20:24 +0330

    refactor the Calculator.java

--- src/main/java/bdd/Calculator.java
+++ src/main/java/bdd/Calculator.java
@@ -2,19 +2,20 @@ package bdd;
 
 public class Calculator {
     public double calculate(double operand1, double operand2, String operator) throws IllegalArgumentException {
-        if (operator.equals("+")) {
-            return operand1 + operand2;
-        } else if (operator.equals("-")) {
-            return operand1 - operand2;
-        } else if (operator.equals("*")) {
-            return operand1 * operand2;
-        }else if (operator.equals("/")) {
-            if (operand2 == 0) {
-                throw new IllegalArgumentException("Division by zero is not allowed.");
-            }
-            return operand1 / operand2;
-        } else {
-            throw new IllegalArgumentException("Invalid operator: " + operator);
+        switch (operator) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                if (operand2 == 0) {
+                    throw new IllegalArgumentException("Division by zero is not allowed.");
+                }
+                return operand1 / operand2;
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + operator);
         }
     }
 }
